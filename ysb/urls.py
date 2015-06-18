197676a4ae99a890e8a6572f513556603bf7f65c
root <root@AY1311151726052873a4Z.(none)>
2015-06-18 19:09:56 +0800

    add favicon.ico

--- ysb/urls.py
+++ ysb/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import RedirectView
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'ysb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$',include('ui.urls')),	
+    url(r'^favicon.ico$', RedirectView.as_view(url='/static/images/favicon.ico')), 	
     url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
     #url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.FILE_UPLOAD_TEMP_DIR}),
